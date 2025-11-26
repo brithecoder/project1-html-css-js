861db6cf624140b97b3228ae07450d3484caf59f
Brianna Gordon <brigordon574@gmail.com>
2025-11-26 12:04:26 -0500

    cleaning code

--- src/services/apiServices.ts
+++ src/services/apiServices.ts
@@ -15,7 +15,6 @@ export async function fetchCountries(): Promise <Country []> {
             throw new customErrors.DataError("API returned a valid response, but the country list is empty.")
         }
         console.log(`  ✅ ✅Successfully fetched ${countryArray.length} countries. ✅ ✅`);
-        console.log(countryArray);
         return countryArray;
     }catch(e){
          console.error( new customErrors.DataError(`Could not get countries: ${e}`));
